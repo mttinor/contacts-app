d07f29a6f0e47af73b6d38d009cb1b95bf74acaa
m.abbaszade <mttinor@yahoo.com>
2024-03-05 10:12:39 +0330

    debug if user choose select then change to defaultValue not allow for write

--- src/components/base/Select.js
+++ src/components/base/Select.js
@@ -9,7 +9,9 @@ const Select = ({ value, title, titleSelected, options, onChangeValue }) => {
         onChange={(e) => onChangeValue(e)}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
-        <option defaultValue> {titleSelected}</option>
+        <option value={""} defaultValue>
+          {titleSelected}
+        </option>
         {options.map(({ value, name }, i) => (
           <option key={i} value={value}>
             {name}
